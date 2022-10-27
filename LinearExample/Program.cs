9f4ba0666a9861b0299822671a2b8160bdd5628c
Dianitsya <dianitasyanandamasta@gmail.com>
2022-10-27 16:21:43 +0700

    add looping untuk jumlah array

--- LinearExample/Program.cs
+++ LinearExample/Program.cs
@@ -15,6 +15,24 @@ namespace LinearExample
         //get the number of element to store in the array
         int i;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 element.\n");
+            }
+            //accept array element
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine(" Enter Array Element ");
+            Console.WriteLine("--------------------------------");
+        }
         static void Main(string[] args)
         {
         }
